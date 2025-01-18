366a3e213bb084910e3036662513453eec3ea7b6
Ajay Teotia <teotiaajay20@gmail.com>
2025-01-18 18:17:34 +0530

    auth-sync-db-and-pingpanda

---
+++ src/action/user.action.ts
@@ -0,0 +1,63 @@
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function SyncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      return;
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    if (existingUser) {
+      return existingUser;
+    }
+
+    // Create new user in the database
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+
+    // Send user information to PingPanda API
+    const response = await fetch(
+      "https://pingpanda-aj.vercel.app/api/v1/events",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.PINGPANDA_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category: "socal-app-new-user",
+          fields: {
+            username: newUser.username,
+            name: newUser.name,
+            email: newUser.email,
+          },
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      console.log("Failed to sync user with PingPanda");
+    }
+
+    return newUser;
+  } catch (error) {
+    console.log("ERROR In SyncUser: ", error);
+  }
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
+import { currentUser } from "@clerk/nextjs/server";
+import { SyncUser } from "@/action/user.action";
 
-export default function Navbar() {
+export default async function Navbar() {
+    const user = await currentUser();
+
+    if (user) await SyncUser();
+
     return (
         <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
             <div className="max-w-7xl mx-auto px-4">
